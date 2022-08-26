a5c16a07d9cb601816fe326e8e7942259bd6b00a
Jakub Kołodziej <94216113+JakubKolodziej21@users.noreply.github.com>
2022-08-26 17:51:55 +0200

    Update of style site

--- gopos-app/src/Layout/Footer.js
+++ gopos-app/src/Layout/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div class="footer">
+      <p>2022 - Projekt został wykonany wyłącznie przez:&nbsp; <a href="https://www.linkedin.com/in/jakub-ko%C5%82odziej-3b342a23a/">Jakub Kołodziej</a>&nbsp; na potrzeby zadania rekrutacyjnego</p> 
 
-export default footer
+    </div>
+  );
+};
+
+export default footer;
--- gopos-app/src/Layout/Header.js
+++ gopos-app/src/Layout/Header.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "../Assets/Images/logo.png";
 
 const Header = () => {
   return (
     <header className="globalBar">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <img alt="logo" src={logo} height="90px" />
+      </div>
       <nav className="nav">
-        <Link to='Page1' className="button">Page1</Link>
-        <Link to='Page2' className="button">Page2</Link>
-        <Link to='Page3' className="button">Page3</Link>
-        <Link to='Page4' className="button">Page4</Link>
-        
+        <Link to="Page1" className="button">
+          Lista produktów
+        </Link>
+        <Link to="Page2" className="button">
+          Lista kategorii
+        </Link>
+        <Link to="Page3" className="button">
+          Edycja produktów
+        </Link>
+        <Link to="Page4" className="button">
+          Edycja kategorii
+        </Link>
+        <Link to="Page5" className="button">
+          Tworzenie nowego produktu i kategorii
+        </Link>
       </nav>
     </header>
   );
