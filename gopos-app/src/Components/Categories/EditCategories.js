2ffb32b1a91fa54fe142f826caa8083318d0f9e8
Jakub Kołodziej <94216113+JakubKolodziej21@users.noreply.github.com>
2022-08-31 02:18:16 +0200

    Final Version 1.1

--- gopos-app/src/Components/Categories/EditCategories.js
+++ gopos-app/src/Components/Categories/EditCategories.js
@@ -1,13 +1,18 @@
 import React from "react";
 import useEditCategories from "./useEditCategories";
+import { useNavigate } from "react-router-dom";
 const EditCategories = (submitForm) => {
 	const { data, handleChange, values, handleSubmit } =
 		useEditCategories(submitForm);
+		let navigate = useNavigate();
+
 	return (
 		<section className='container'>
 			<div className='space'>
 				<div className='symbol'>&#10002;</div>
+				
 			</div>
+			<h1>Edytuj nazwę kategorii</h1>
 			<input
 				onChange={handleChange}
 				type='text'
@@ -15,6 +20,7 @@ const EditCategories = (submitForm) => {
 				name='name'
 				placeholder='Podaj nazwę nowej kategori'
 			/>
+			<button onClick={() => navigate(-1)}>Cofnij</button>
 			<button onClick={handleSubmit}>Zatwierdź</button>
 
 		</section>
--- gopos-app/src/Components/Products/EditListOfProduct.js
+++ gopos-app/src/Components/Products/EditListOfProduct.js
@@ -4,45 +4,44 @@ import useEditListOfProduct from "./useEditListOfProduct";
 import { useNavigate } from "react-router-dom";
 
 const EditListOfProduct = (submitForm) => {
-	let navigate = useNavigate();
-	const { handleChange, values, handleSubmit, categoryList, handleCategory } =
-		useEditListOfProduct(submitForm);
+  let navigate = useNavigate();
+  const { handleChange, values, handleSubmit, categoryList, handleCategory } =
+    useEditListOfProduct(submitForm);
 
-	return (
-		<section className='container'>
-			<div className='space'>
-				<div className='symbol'>&#9881;</div>
-			</div>
-			<h1>Stwórz Produkt & Kategorię</h1>
-			<button onClick={() => navigate(-1)}>Cofnij</button>
-			<input
-				onChange={handleChange}
-				type='text'
-				value={values.name}
-				name='name'
-				placeholder='Podaj nazwę nowego produktu'
-			/>
-			<CreatableSelect
-			theme={(theme) => ({
-				...theme,
-				borderRadius: 0,
-				
-	  
-				colors: {
-				  ...theme.colors,
-				  primary25: "#1a86e0",
-				  primary: "#434e5e",
-				  color: "black",
-				},
-			  })}
-				isClearable
-				onChange={handleCategory}
-				options={categoryList}
-			/>
+  return (
+    <section className="container">
+      <div className="space">
+        <div className="symbol">&#9881;</div>
+      </div>
+      <h1>Stwórz Produkt & Kategorię</h1>
+      <button onClick={() => navigate(-1)}>Cofnij</button>
+      <input
+        onChange={handleChange}
+        type="text"
+        value={values.name}
+        name="name"
+        placeholder="Podaj nazwę nowego produktu"
+      />
+      <CreatableSelect
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
 
-			<button onClick={handleSubmit}>Zatwierdź</button>
-		</section>
-	);
+          colors: {
+            ...theme.colors,
+            primary25: "#1a86e0",
+            primary: "#434e5e",
+            color: "black",
+          },
+        })}
+        isClearable
+        onChange={handleCategory}
+        options={categoryList}
+      />
+
+      <button onClick={handleSubmit}>Zatwierdź</button>
+    </section>
+  );
 };
 
 export default EditListOfProduct;
